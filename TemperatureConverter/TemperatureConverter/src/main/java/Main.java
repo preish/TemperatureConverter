4b968d8e290b7a10852dfd237deb1e68216e05bd
Juan Sulse <juansulse@gmail.com>
2022-01-18 17:39:45 -0600

    Started working on getting the main functionality of the program done before switching to a Web App.

--- TemperatureConverter/TemperatureConverter/src/main/java/Main.java
+++ TemperatureConverter/TemperatureConverter/src/main/java/Main.java
@@ -1,15 +1,47 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int FAHRENHEIT_CHOICE = 1;
+		
 		// Display greeting message
 		printGreeting();
+		
+		try {
+			// Get user choice
+			int userChoice = getUserChoice(scanner);
+		}
+		catch (Exception e) {
+			System.out.println("Please enter a whole number.");
+		}
+		
+		// Get the temperature
+		float userTemperature = getTemperature(scanner);
+		
+		
 
 	}
 	
 	public static void printGreeting() {
 		System.out.println("Welcome to Temperature Converter 3000!");
 		System.out.println("This program converts a temperature from Celsius to Fahrenheit and vice versa.");
+		System.out.println("1 - Convert Celcius to Fahrenheit");
+		System.out.println("2 - Convert Fahrenheit to Celsius");
+		System.out.print("Please type in either 1 or 2: ");
+	}
+	
+	public static int getUserChoice(Scanner scanner) {
+		return scanner.nextInt();
+	}
+	
+	public static float getTemperature(Scanner scanner) {
+		return scanner.nextFloat();
+	}
+	
+	public static float convertTemperature(float userTemperature) {
+		return 0.0f;
 	}
 
 }
