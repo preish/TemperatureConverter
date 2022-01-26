8d5871dad0c59b86e12e6d977e3bb62ee67c1b3f
Juan Sulse <juansulse@gmail.com>
2022-01-26 16:14:01 -0600

    Continued working on JS function to calculate the temp conversion.

--- TemperatureConverter/TemperatureConverter/src/main/webapp/temperature-converter.js
+++ TemperatureConverter/TemperatureConverter/src/main/webapp/temperature-converter.js
@@ -1,5 +1,12 @@
 // Indicates that the Click Me button has been pressed
 function calculateNewTemperature() {
-	var temperatureScale;
-    alert('You pressed the button!')
+	var temperatureScale = document.getElementsByTagName('input');
+	
+	for(i = 0; i < temperatureScale.length; i++) {
+		if(temperatureScale[i].type = "radio") {
+			if(temperatureScale[i].checked)
+				if(temperatureScale[i].localName = "Fahrenheit")
+				alert('You selected Fahrenheit!')
+		}
+	}
 }
