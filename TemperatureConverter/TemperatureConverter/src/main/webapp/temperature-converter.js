8f7de24d421e0a48fea7fbb934795f9859bbd35d
Juan Sulse <juansulse@gmail.com>
2022-01-28 15:42:54 -0600

    Finished adding JS functionality.

--- TemperatureConverter/TemperatureConverter/src/main/webapp/temperature-converter.js
+++ TemperatureConverter/TemperatureConverter/src/main/webapp/temperature-converter.js
@@ -1,12 +1,20 @@
-// Indicates that the Click Me button has been pressed
+// Calculates the new temperature once the Go button is clicked
 function calculateNewTemperature() {
-	var temperatureScale = document.getElementsByTagName('input');
+	var chosenTemperatureScale;
+	var userTemperature = document.getElementById('userTemperature').value;
 	
-	for(i = 0; i < temperatureScale.length; i++) {
-		if(temperatureScale[i].type = "radio") {
-			if(temperatureScale[i].checked)
-				if(temperatureScale[i].localName = "Fahrenheit")
-				alert('You selected Fahrenheit!')
+	if(userTemperature == "")
+		alert("Please enter a temperature to convert.");
+	else {
+		if(document.getElementById('fTemperature').checked) {
+			chosenTemperatureScale = " degrees Fahrenheit.";
+			var result = ((userTemperature * 9) / 5) + 32;
+			alert(userTemperature + " degrees Celsius converts to " + result + chosenTemperatureScale);
+		}
+		else if(document.getElementById('cTemperature').checked) {
+			chosenTemperatureScale = " degrees Fahrenheit.";
+			var result = ((userTemperature - 32) * 5) / 9;
+			alert(userTemperature + " degrees Celsius converts to " + result + chosenTemperatureScale);	
 		}
 	}
 }
